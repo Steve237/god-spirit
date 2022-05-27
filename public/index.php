22f36b56fc60248c37c9cbff3d694599ed5285cf
Essama Mgba Franck Steve <adouessono@yahoo.fr>
2022-05-27 23:05:09 +0200

    add procfile

--- public/index.php
+++ public/index.php
@@ -4,9 +4,6 @@ use App\Kernel;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
-var_dump("salut");
-die();
-
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
 };
