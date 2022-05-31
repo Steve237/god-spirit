9e907a53e6e074abe6b9accf3a534c1420f6e716
Essama Mgba Franck Steve <adouessono@yahoo.fr>
2022-05-31 12:23:30 +0200

    update composer json and index.php

--- public/index.php
+++ public/index.php
@@ -5,7 +5,7 @@ use App\Kernel;
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
 error_reporting(E_ALL);
-ini_set('display_errors', '1');
+ini_set('display_errors', 'On');
 
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
