08864ed6e684a14e5020fceb7e48c282b224b416
Essama Mgba Franck Steve <adouessono@yahoo.fr>
2022-05-31 11:57:48 +0200

    update index.php to show errors in prod

--- public/index.php
+++ public/index.php
@@ -4,6 +4,9 @@ use App\Kernel;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
+
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
 };
